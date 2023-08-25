f8547c018000598b11a11a78753f16803d0dcb18
rotiuskyi <roman.otiuskyi@gmail.com>
2023-08-25 20:53:23 +0300

    Create http client factory

---
+++ webui/src/@infra.util/httpClientFactory.ts
@@ -0,0 +1,45 @@
+type RequestOptions = Omit<RequestInit, "method">;
+
+interface BeforeHook {
+  (init: RequestInit): RequestInit;
+}
+
+interface AfterHook {
+  (response: Response): Response;
+}
+
+interface HttpClientOptions {
+  baseUrl?: string;
+  initRequestOptions?: RequestOptions;
+  before?: BeforeHook;
+  after?: AfterHook;
+}
+
+type Method = "GET" | "POST" | "PUT" | "DELETE";
+
+function httpClientFactory({
+  baseUrl = "",
+  initRequestOptions,
+  after,
+  before = identity
+}: HttpClientOptions = {}) {
+  function requestFactory(method: Method) {
+    return (uri: string, requestOptions: RequestOptions = {}) => {
+      const init: RequestInit = before({ ...initRequestOptions, ...requestOptions, method });
+      return fetch(baseUrl + uri, init).then(after);
+    };
+  }
+
+  return {
+    get: requestFactory("GET"),
+    post: requestFactory("POST"),
+    put: requestFactory("PUT"),
+    delete: requestFactory("DELETE")
+  };
+}
+
+function identity<T extends any>(arg: T): T {
+  return arg;
+}
+
+export default httpClientFactory;
